dfca02f1f92708e2da28f3ebe87b65f33c0f17fa
Parag <paragwayangankar@gmail.com>
2025-04-15 17:19:52 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
+connectDB();
+
+// Basic route
+globalThis._server_root = __dirname;
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
